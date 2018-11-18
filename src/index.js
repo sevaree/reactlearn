5db5dc30a2c7df61053817b086179fd21ce1836b
chen <sevaree@126.com>
2018-11-18 11:49:07 +0800

    初次提交

--- src/index.js
+++ src/index.js
@@ -2,34 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-	constructor(props) {
-	    super(props);
-	    this.state = {
-	        value : null
-        }
-    }
-
-	render() {
-		return (
-			<button className="square" onClick={() => {this.setState({value : "X"})}}>
-			{this.state.value}
-			</button>
-		);
-	}
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
+
+    handlerClick(i) {
+    	const squares = this.state.squares.slice();
+    	if(calculateWinner(squares) || squares[i]) {
+    	    return;
+        }
+    	squares[i] = this.state.xIsNext?'x':'o';
+    	this.setState({
+			squares : squares,
+			xIsNext : !this.state.xIsNext
+    	});
+	}
+
 	renderSquare(i) {
-		return <Square value={i}/>;
+		return (
+			<Square
+				value={this.props.squares[i]}
+				onClick={()=> this.props.onClick(i)}
+		/>);
 	}
 	
 	render() {
-		const status = 'Next player : X';
-		
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 				{this.renderSquare(0)}
 				{this.renderSquare(1)}
@@ -47,23 +52,83 @@ class Board extends React.Component {
 				</div>
 			</div>
 		);
-	}
+	};
+
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            history: [{
+                squares:Array(9).fill(null),
+            }],
+            stepNumber:0,
+            xIsNext:true,
+        }
+    }
 	render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        });
+
+        let status;
+        if(winner) {
+            status = 'Winner is ' + winner
+        } else {
+            status = 'Next player is ' + (this.state.xIsNext?'x':'o');
+        }
 		return (
 			<div className="game">
 				<div className="game-borad">
-					<Board />
+					<Board
+                        squares={current.squares}
+                        onClick={(i) => this.handlerClick(i)}/>
 				</div>
 				<div className="game-info">
-					<div>{/*status*/}</div>
-					<ol>{/*TODO*/}</ol>
+					<div>{status}</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
 	}
+
+	jumpTo(step) {
+        this.setState({
+            stepNumber :step,
+            xIsNext : (step % 2) === 0,
+        });
+    }
+
+	handlerClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber +1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        const winner = calculateWinner(squares);
+
+        if(winner || squares[i]) {
+            return
+        }
+
+        squares[i] = this.state.xIsNext?'x':'o';
+        this.setState({
+            history: history.concat([{
+                squares:squares,
+            }]),
+            stepNumber : history.length,
+            xIsNext: !this.state.xIsNext
+        });
+
+    }
 }
 
 
@@ -71,3 +136,24 @@ ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for(let i=0; i<lines.length;i++) {
+        const [a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[a] && squares[b] === squares[a] && squares[c]) {
+            return squares[a]
+        }
+    }
+    return null;
+}
